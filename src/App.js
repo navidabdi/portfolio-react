deff96efb5778f0cdabe0e47e1dc20aa9df46a71
navidabdi <artsanaat@gmail.com>
2022-01-04 22:09:31 +0330

    5. Add Some Style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import react from "react";
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
 import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,8 +1,11 @@
 import react from "react";
 import home1 from "../img/home1.png";
+// Styled
+import styled from "styled-components";
+
 const AboutSection = () => {
   return (
-    <div className="about">
+    <AboutStyle>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -26,8 +29,38 @@ const AboutSection = () => {
       <div className="image">
         <img src={home1} alt="guy with a camera" />
       </div>
-    </div>
+    </AboutStyle>
   );
 };
+
+// Styled Component
+const AboutStyle = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+
+  .description {
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+      font-weight: lighter;
+    }
+  }
+  .image {
+    flex: 1;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 80vh;
+      object-fit: cover;
+    }
+  }
+  .hide {
+    overflow: hidden;
+  }
+`;
 
 export default AboutSection;
