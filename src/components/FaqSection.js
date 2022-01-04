2afe3d209a7c641592ebfb4b60c5f57a42d323f6
navidabdi <artsanaat@gmail.com>
2022-01-04 21:37:19 +0330

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import react from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+            cumque, explicabo libero nesciunt.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>How Is The Daily Schedule?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+            cumque, explicabo libero nesciunt.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Diffrent Payment Metod?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+            cumque, explicabo libero nesciunt.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Kind Of Products Do You Offer?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+            cumque, explicabo libero nesciunt.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,11 +2,13 @@ import react from "react";
 // Import The About Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
