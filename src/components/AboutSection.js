405859748a50e592b7e5ca33ddbef6abfc3ca5ab
navidabdi <artsanaat@gmail.com>
2022-01-05 16:56:05 +0330

    12. Start With Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,23 +4,51 @@ import home1 from "../img/home1.png";
 import styled from "styled-components";
 import { StyleLayout } from "../styles.js";
 
+// Frame Motion
+import { motion } from "framer-motion";
+
 const AboutSection = () => {
+  const titleAnimate = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 1 } },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut",
+        staggerChildren: 0.5,
+        when: "beforeChildren",
+      },
+    },
+  };
+
   return (
     <StyleLayout>
       <div className="description">
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <div className="hide">
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnimate} initial="hidden" animate="show">
+              We work to make
+            </motion.h2>
           </div>
           <div className="hide">
-            <h2>
+            <motion.h2 variants={titleAnimate}>
               your <span>dreams</span>
-            </h2>
+            </motion.h2>
           </div>
           <div className="hide">
-            <h2>come true.</h2>
+            <motion.h2 variants={titleAnimate}>come true.</motion.h2>
           </div>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography idea that you have, we
           have the best artist in the word that you cannot imagain.
