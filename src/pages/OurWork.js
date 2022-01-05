2ce6527126611c2b3fc5c928753394859729bf54
navidabdi <artsanaat@gmail.com>
2022-01-05 13:21:18 +0330

    10. Our Work

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,12 +1,61 @@
 import react from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 // Import The OurWork Page Components
+
+// Images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 const OurWork = () => {
   return (
-    <div className="our-work">
-      <h1>Our Work</h1>
-    </div>
+    <StyledWork>
+      <div className="movie">
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="athlete" />
+        </Link>
+      </div>
+      <div className="movie">
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="theracer" />
+        </Link>
+      </div>
+      <div className="movie">
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="goodtimes" />
+        </Link>
+      </div>
+    </StyledWork>
   );
 };
+
+const StyledWork = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  .movie {
+    padding-bottom: 10rem;
+    h2 {
+      padding: 1rem 0;
+    }
+    .line {
+      height: 0.5rem;
+      background: #ccc;
+      margin-bottom: 3rem;
+    }
+    img {
+      width: 100%;
+      height: 70vh;
+      object-fit: cover;
+    }
+  }
+`;
 
 export default OurWork;
